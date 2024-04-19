857bc558ae0bb3c6d87777ca49a55456bc795691
wuppo <wobjelsm@gmail.com>
2024-04-19 16:56:29 +0200

    fix: template enemy class

--- src/main/java/com/github/fixitfelixjr/entities/Enemy.java
+++ src/main/java/com/github/fixitfelixjr/entities/Enemy.java
@@ -17,25 +17,48 @@ public class Enemy extends DynamicSpriteEntity implements KeyListener, SceneBord
 {
 
     public final static String SPRITE_IMAGE = "sprites/Ralph_front_sprite.png";
-    public final static Size SIZE = new Size(50, 68);
-    public final static int[] SPRITE_ROWS_COLS = {1,1};
-    public final static Position INITIAL_POSITION = Position.ENEMY_INITIAL_POSITION;
+    public final static Size SIZE = new Size(180, 195);
+    public final static int[] SPRITE_ROWS_COLS = {1, 1};
+    public final static Position INITIAL_POSITION = Position.PLAYER_INITIAL_POSITION;
 
+    private static final int MAX_HEALTH = 10;
+    private static final double MOVE_SPEED = 7.5;
+    private static final double JUMP_STRENGTH = 20;
+    private static final double GRAVITY_CONSTANT = 0.5;
 
-    private int initialHealth = 1;
+    private int health;
     private boolean isJumping = false;
 
     public Enemy()
     {
         super(SPRITE_IMAGE, INITIAL_POSITION.getCoordinate(), SIZE, SPRITE_ROWS_COLS[0], SPRITE_ROWS_COLS[1]);
-
-        setGravityConstant(0.034);
+        this.health = MAX_HEALTH;
+        setGravityConstant(GRAVITY_CONSTANT);
     }
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys)
     {
+        if (!isJumping && (pressedKeys.contains(KeyCode.UP) || pressedKeys.contains(KeyCode.W) || pressedKeys.contains(KeyCode.SPACE))) {
+            isJumping = true;
+            setMotion(JUMP_STRENGTH, Direction.UP.getValue());
+            setGravityConstant(GRAVITY_CONSTANT);
+        }
+
+        if (pressedKeys.contains(KeyCode.LEFT) || pressedKeys.contains(KeyCode.A)) {
+            setMotion(MOVE_SPEED, Direction.LEFT.getValue());
+            setCurrentFrameIndex(0);
+        } else if (pressedKeys.contains(KeyCode.RIGHT) || pressedKeys.contains(KeyCode.D)) {
+            setMotion(MOVE_SPEED, Direction.RIGHT.getValue());
+            setCurrentFrameIndex(1);
+        } else if (pressedKeys.contains(KeyCode.DOWN) || pressedKeys.contains(KeyCode.S)) {
+            setMotion(MOVE_SPEED, Direction.DOWN.getValue());
+        } else if (pressedKeys.isEmpty()) {
+            setSpeed(0);
+        }
     }
+
+
 
     @Override
     public void onCollision(List<Collider> collidingObjects)
@@ -46,7 +69,29 @@ public class Enemy extends DynamicSpriteEntity implements KeyListener, SceneBord
     @Override
     public void notifyBoundaryTouching(SceneBorder border)
     {
-    }
 
+        if (border == SceneBorder.BOTTOM) {
+            isJumping = false;
+            setGravityConstant(0);
+            setMotion(0, 0);
+        }
+
+        setSpeed(0);
+        switch (border) {
+            case TOP:
+                setAnchorLocationY(1);
+                break;
+            case BOTTOM:
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+                break;
+            case LEFT:
+                setAnchorLocationX(1);
+                break;
+            case RIGHT:
+                setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+            default:
+                break;
+        }
+    }
 
 }
