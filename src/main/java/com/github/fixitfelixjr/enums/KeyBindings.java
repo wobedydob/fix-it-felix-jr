9e72d6c678f4fdfa2013c7388d27b027b9415868
wuppo <wobjelsm@gmail.com>
2024-04-28 20:35:18 +0200

    feat: cleaned code for movement

---
+++ src/main/java/com/github/fixitfelixjr/enums/KeyBindings.java
@@ -0,0 +1,27 @@
+package com.github.fixitfelixjr.enums;
+
+import javafx.scene.input.KeyCode;
+
+public enum KeyBindings
+{
+
+    UP(KeyCode.W),
+    DOWN(KeyCode.S),
+    LEFT(KeyCode.A),
+    RIGHT(KeyCode.D),
+    REPAIR(KeyCode.ENTER),
+    DESTROY(KeyCode.BACK_SPACE);
+
+    private final KeyCode keyCode;
+
+    KeyBindings(KeyCode keyCode)
+    {
+        this.keyCode = keyCode;
+    }
+
+    public KeyCode getKeyCode()
+    {
+        return keyCode;
+    }
+
+}
