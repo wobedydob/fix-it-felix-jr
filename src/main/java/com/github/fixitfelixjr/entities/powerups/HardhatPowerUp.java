d2e869302f20bdc28ebb0348419f4e50814ad9a2
leko <jelsmaloek@gmail.com>
2024-04-30 14:51:36 +0200

    Feat: added hardhat powerup

--- src/main/java/com/github/fixitfelixjr/entities/powerups/HardhatPowerUp.java
+++ src/main/java/com/github/fixitfelixjr/entities/powerups/HardhatPowerUp.java
@@ -4,16 +4,16 @@ import com.github.fixitfelixjr.scenes.LevelScene;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 
-public class PiePowerUp extends PowerUp
+public class HardhatPowerUp extends PowerUp
 {
-    public static final String SPRITE_IMAGE = "sprites/pie_powerup.png";
-    public static final double WIDTH = 22;
-    public static final double HEIGHT = 13;
+    public static final String SPRITE_IMAGE = "sprites/hardhat_powerup.png";
+    public static final double WIDTH = 10;
+    public static final double HEIGHT = 7;
     public static final Size SIZE = new Size(WIDTH * LevelScene.SPRITE_SIZE_APPLIER,  HEIGHT * LevelScene.SPRITE_SIZE_APPLIER);
-    public static final int[] SPRITE_ROWS_COLS = {1, 2};
+    public static final int[] SPRITE_ROWS_COLS = {1, 1};
     public static final int DURATION = 5;
 
-    public PiePowerUp(Coordinate2D position)
+    public HardhatPowerUp(Coordinate2D position)
     {
         super(SPRITE_IMAGE, DURATION, position, SIZE, SPRITE_ROWS_COLS[0], SPRITE_ROWS_COLS[1]);
     }
